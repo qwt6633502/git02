e50ff33c03ca1da4a1a8c2d1f1df6dcb9e7b6f63
qwt <qwt8888888@gmail.com>
2019-08-28 21:19:38 +0800

    邱帅韬的代码

---
+++ git03/src/main/java/Seu.java
@@ -0,0 +1,8 @@
+/**
+ * @program: Git02
+ * @description:
+ * @author:
+ * @create: 2019-08-28 21:12
+ **/
+public class Seu {
+}
--- git03/src/main/java/User.java
+++ git03/src/main/java/User.java
@@ -6,4 +6,6 @@
  **/
 public class User {
     private String name;
+    private int age;
+
 }
